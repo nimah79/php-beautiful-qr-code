2322e8936bf9d71fe632c294af59ab2b03942661
Nima HeydariNasab <nima.heydari79@yahoo.com>
2019-04-04 17:51:03 +0430

    Make files structure better

---
+++ demo.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Beautiful QR Code generator
+ * By NimaH79
+ * NimaH79.ir
+ */
+
+require __DIR__.'/phpqrcode/beautiful-qr-code.php';
+
+$text = '@Radio_Nima';
+$backgroundColor = 'ffffff';
+$primaryColor = '303030';
+$secondaryColor = 'ca012f';
+$scale = 3; // Higher scale, higher quality and slower speed
+$type = 'png'; // PNG is better, trust me :)
+
+header('Content-type: image/png');
+
+echo generateBeautifulQRCode($text, $backgroundColor, $primaryColor, $secondaryColor, $scale, $type);
--- phpqrcode/beautiful-qr-code.php
+++ phpqrcode/beautiful-qr-code.php
@@ -1,23 +1,6 @@
 <?php
 
-/*
- * Beautiful QR Code generator
- * By NimaH79
- * NimaH79.ir
-*/
-
-require 'phpqrcode/qrlib.php';
-
-$text = '@Radio_Nima';
-$backgroundColor = 'ffffff';
-$primaryColor = '303030';
-$secondaryColor = 'ca012f';
-$scale = 3; // Higher scale, higher quality and slower speed
-$type = 'png'; // PNG is better, trust me :)
-
-header('Content-type: image/png');
-
-echo generateBeautifulQRCode($text, $backgroundColor, $primaryColor, $secondaryColor, $scale, $type);
+require __DIR__.'/qrlib.php';
 
 function generateBeautifulQRCode($text, $backgroundColor, $primaryColor, $secondaryColor, $scale, $type)
 {
